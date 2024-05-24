cf7a47eee64d9d2336c46d145eeb737139f84187
ritika shrestha <akitirsth@gmail.com>
2024-05-23 23:28:27 -0400

    some changes

--- renderer/js/main_renderer.js
+++ renderer/js/main_renderer.js
@@ -5,8 +5,10 @@ const Save = document.getElementById('save');
 
 
 const canvas = document.getElementById('canvas1');
+const canvas2 = document.getElementById('canvas2');
 //to acess all the canvas build in methods
 const ctx = canvas.getContext('2d');
+const ctx2 = canvas2.getContext('2d');
 let scale=1;
 let path1;
 const image = new Image();
@@ -38,32 +40,23 @@ function loadImage(e){
     $("#slider").on('change input',function(){
       console.log(this.value);
       $("#slider-label").text(this.value);
-      let pre_value=0;
 
-      if(this.value > pre_value ){
-        scale=1;        
-      }else if(this.value < pre_value){
-        scale=-1;
-      }else if(this.value ==0){
-        scale =0;
-      }
-      pre_value=this.value;
+      // //GREY SCALLING
+      // for (let i=0;i<scannedData.length;i+=4){
+      // // rgb
+      //   const total= scannedData[i]+ scannedData[i+1]+scannedData[i+2];
+      //   const averageColorValue = total/3;
+      //   scannedData[i] = averageColorValue + scale;
+      //   scannedData[i+1]= averageColorValue + scale;
+      //   scannedData[i+2] = averageColorValue + scale;
+      // }
+      // scannedImg.data= scannedData;
 
-      //GREY SCALLING
-      for (let i=0;i<scannedData.length;i+=4){
-      // rgb
-        const total= scannedData[i]+ scannedData[i+1]+scannedData[i+2];
-        const averageColorValue = total/3;
-        scannedData[i] = averageColorValue + scale;
-        scannedData[i+1]= averageColorValue + scale;
-        scannedData[i+2] = averageColorValue + scale;
-      }
-      scannedImg.data= scannedData;
+      // ctx.putImageData(scannedImg,0,0);
 
-      ctx.putImageData(scannedImg,0,0);
-
-      // ctx.filter=`blur(${scale}px)`;
-      // ctx.drawImage(image,0,0);
+      ctx.filter=`blur(${this.value}px)`;
+      ctx.drawImage(image,0,0);
+      // console.log(ctx.getImageData(0,0,canvas.width,canvas.height));
     });
 
 
@@ -76,20 +69,20 @@ function loadImage(e){
 
 img_selector.addEventListener('change',loadImage);
 
-// Save.addEventListener('click',function(){
-//   const url=ctx.getImageData(0,0,canvas.width,canvas.height).data;
-//   console.log(url);
-//   console.log(path1);
-//   // image.src=url;
-//   const width=10;
-//   const height=10;
+Save.addEventListener('click',function(){
+  const url=canvas.toDataURL();
+  console.log(canvas.toBlob());
+  const image2 = new Image();
+    image2.src= url;
+  image2.onload = function (){
   
-//   ipcRenderer.send('image:resiz',{
-//     url,
-//     path1,
-//     height,
-// });
-// })
+    canvas2.width= this.width;
+    canvas2.height= this.height;
+
+    ctx2.drawImage(image2,0,0);    
+  }
+
+})
 
 
 
